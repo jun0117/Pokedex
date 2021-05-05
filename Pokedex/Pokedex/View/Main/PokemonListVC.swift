8b23b0f03a24d184e9a8d9f77559bb19d7e2ecf5
Junsung Lee <s9174844@gmail.com>
2021-05-06 03:31:39 +0900

    PokemonListView 추가

--- Pokedex/Pokedex/View/Main/PokemonListVC.swift
+++ Pokedex/Pokedex/View/Main/PokemonListVC.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class PokemonListVC: UIViewController {
+    private var listView: PokemonListView { view as! PokemonListView }
     var listVM: PokemonListVM!
+    
+    override func loadView() {
+        view = PokemonListView()
+    }
         
     override func viewDidLoad() {
         super.viewDidLoad()
