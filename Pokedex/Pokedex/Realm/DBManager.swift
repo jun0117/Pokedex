3c847109e457ccd3bc85dc37d90500c1122f5e10
Junsung Lee <s9174844@gmail.com>
2021-05-05 20:03:37 +0900

    DBManager class 추가

---
+++ Pokedex/Pokedex/Realm/DBManager.swift
@@ -0,0 +1,39 @@
+//
+//  DBManager.swift
+//  Pokedex
+//
+//  Created by 이준성 on 2021/05/05.
+//
+
+import SwiftyJSON
+import RealmSwift
+import Then
+
+class DBManager {
+    private let realm = try! Realm()
+    
+    func insertPokemonList(_ page: Int, jsonList: [JSON]) {
+        let pokemonList = jsonList.map { json -> Pokemon in
+            return Pokemon().then {
+                $0.name = json["name"].stringValue
+                $0.url = json["url"].stringValue
+                $0.page = page
+            }
+        }
+        
+        try! realm.write {
+            realm.add(pokemonList, update: .modified)
+        }
+    }
+    
+    func getPokemonList(_ page: Int) -> [Pokemon] {
+        let pokemonList = realm.objects(Pokemon.self).filter { $0.page == page }
+        return Array(pokemonList)
+    }
+    
+    func getAllPokemonList(_ page: Int) -> [Pokemon] {
+        let pokemonList = realm.objects(Pokemon.self).filter { $0.page <= page }
+        return Array(pokemonList)
+    }
+}
+
