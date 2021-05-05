2e20d9f867e03b0110081532cae81ca0d01d7c35
Junsung Lee <s9174844@gmail.com>
2021-05-05 17:26:26 +0900

    APIService 추가

---
+++ Pokedex/Pokedex/Network/APIService.swift
@@ -0,0 +1,48 @@
+//
+//  APIService.swift
+//  Pokedex
+//
+//  Created by 이준성 on 2021/05/04.
+//
+
+import Moya
+
+enum APIService {
+    case pokemonResponse(Int)
+    case pokemonInfo(String)
+}
+
+extension APIService: TargetType {
+    var baseURL: URL { URL(string: "https://pokeapi.co/api/v2/")! }
+    var path: String {
+        switch self {
+        case .pokemonResponse(_):
+            return "pokemon/"
+            
+        case let .pokemonInfo(name):
+            return "pokemon/\(name)"
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case let .pokemonResponse(page):
+            let PAGE_SIZE = 20
+            let params: [String: Any?] = [
+                "limit": PAGE_SIZE,
+                "offset": page * PAGE_SIZE
+            ]
+            
+            return .requestParameters(
+                parameters: params.compactMapValues { $0 },
+                encoding: URLEncoding.queryString
+            )
+            
+        default: return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? { [:] }
+    var method: Method { .get }
+    var sampleData: Data { Data() }
+}
