f02f6125ae552bf7065336a0e1b10dda998aecc5
Junsung Lee <s9174844@gmail.com>
2021-05-08 22:48:58 +0900

    PokemonInfo Object 추가

---
+++ Pokedex/Pokedex/Model/PokemonInfo.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonInfo.swift
+//  Pokedex
+//
+//  Created by 이준성 on 2021/05/07.
+//
+
+import RealmSwift
+
+class PokemonInfo: Object {
+    @objc dynamic var index: Int = 0
+    @objc dynamic var hp: Int = 0 // swiftlint:disable:this identifier_name
+    @objc dynamic var weight: Int = 0
+    @objc dynamic var height: Int = 0
+    @objc dynamic var defense: Int = 0
+    @objc dynamic var attack: Int = 0
+    @objc dynamic var speed: Int = 0
+    var typeList: List<String> = .init()
+
+    override class func primaryKey() -> String? {
+        "index"
+    }
+
+    var pokemonTypeList: [PokemonType] {
+        typeList.map {
+            PokemonType(rawValue: $0) ?? .normal
+        }
+    }
+}
